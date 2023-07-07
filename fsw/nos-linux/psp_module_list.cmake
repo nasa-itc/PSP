949cbe36aac9ce17c3b6bbe7af8fe1c238da8bcd
Lucas, John P <John.P.Lucas@nasa.gov>
2023-07-07 12:22:54 -0400

    [nasa/nos3#88] Attempt to resolve nos time driver issue;

--- fsw/nos-linux/psp_module_list.cmake
+++ fsw/nos-linux/psp_module_list.cmake
@@ -1,8 +1,8 @@
 # This is a list of modules that is included as a fixed/base set
 # when this PSP is selected.  They must exist under fsw/modules
 
-soft_timebase
-timebase_posix_clock
-eeprom_mmap_file
-ram_notimpl
+eeprom_notimpl
 port_notimpl
+ram_notimpl
+#soft_timebase
+#timebase_posix_clock
